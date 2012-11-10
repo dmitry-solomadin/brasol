0fdf49f5e800800011c6819426ab94a3cb63f202
Dmitry Solomadin <dmitry.solomadin@gmail.com>
2012-11-10 19:52:31 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brasol::Application.config.secret_token = '08010c193fdfec38977011aa839c99c00cfa41b2e4021156206269a6a5b7a27fd49ac1068351e615918eb882abdc3181801afda618070c2cce9fe378bc3c21d5'
