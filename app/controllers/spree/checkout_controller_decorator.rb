429e779c057ee45d4df91760849afc756fcadc01
Dmitry Solomadin <dmitry.solomadin@gmail.com>
2013-01-05 16:03:26 +0200

    #14, #13. Fixed order table design. #5 Removed zipcode from checkout process. Some checkout fixes.

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,17 +1,27 @@
 Spree::CheckoutController.class_eval do
   def before_address
-    @order.bill_address ||= @order.user.bill_address || Spree::Address.default
-    @order.ship_address ||= @order.user.ship_address || Spree::Address.default
+    if @order.user
+      @order.bill_address ||= @order.user.bill_address || Spree::Address.default
+      @order.ship_address ||= @order.user.ship_address || Spree::Address.default
+    else
+      @order.bill_address = Spree::Address.default
+      @order.ship_address = Spree::Address.default
+    end
 
-    @order.bill_address.firstname = @order.user.firstname if @order.user.firstname.present?
-    @order.bill_address.lastname = @order.user.lastname if @order.user.lastname.present?
+    if @order.user
+      @order.bill_address.firstname = @order.user.firstname if @order.user.firstname.present?
+      @order.bill_address.lastname = @order.user.lastname if @order.user.lastname.present?
+    end
   end
 
   def after_complete
-    @order.user.update_attribute(:bill_address, @order.bill_address) # Update last billing address used.
-    @order.user.update_attribute(:ship_address, (@order.bill_address == @order.ship_address ? nil : @order.ship_address)) # Update last shipping address used.
-    @order.user.update_attribute(:firstname, @order.bill_address.firstname)
-    @order.user.update_attribute(:lastname, @order.bill_address.lastname)
+    if @order.user
+      @order.user.update_attribute(:bill_address, @order.bill_address) # Update last billing address used.
+      @order.user.update_attribute(:ship_address, (@order.bill_address == @order.ship_address ? nil : @order.ship_address)) # Update last shipping address used.
+      @order.user.update_attribute(:firstname, @order.bill_address.firstname)
+      @order.user.update_attribute(:lastname, @order.bill_address.lastname)
+    end
+
     session[:order_id] = nil
   end
 end
--- app/overrides/address_mod.rb
+++ app/overrides/address_mod.rb
@@ -6,3 +6,9 @@ Deface::Override.new(:virtual_path => "spree/checkout/_address",
                      :name => "country_ukraine",
                      :replace => "code[erb-loud]:contains(\"bill_form.collection_select :country_id, available_countries, :id, :name, {}, {:class => 'required'}\")",
                      :text => "<%= bill_form.hidden_field :country_id, :value => ukraine.id %>")
+Deface::Override.new(:virtual_path => 'spree/checkout/_address',
+                     :name => 'remove_zipcode',
+                     :remove => '#szipcode')
+Deface::Override.new(:virtual_path => 'spree/checkout/_address',
+                     :name => 'remove_zipcode2',
+                     :remove => '#bzipcode')
